78858393b8403fd99eac46be09da11470fa2ba51
edonet <edonet@163.com>
2018-11-12 19:00:31 +0800

    update: history

--- src/index.js
+++ src/index.js
@@ -33,20 +33,45 @@ let history = createHistory();
  */
 class App extends Component {
 
+    /* 初始化组件 */
     constructor(props, ...args) {
         super(props, ...args);
 
-        this.state = { reoute: null };
+        // 定义属性
+        this.state = { reoute: history.state };
 
+        // 监听路由变更
         history.subscribe(route => this.setState({ route }));
     }
 
+    /* 渲染组件 */
     render() {
-        return (
-            <div>
-                <div>{ JSON.stringify(this.state.route) }</div>
-            </div>
-        );
+
+        // 渲染结果
+        if (this.state.route) {
+            return (
+                <div>
+                    <p>url: { this.state.route.url }</p>
+                    <p>method: { history.method }</p>
+                    <p>query: { JSON.stringify(this.state.route.query) }</p>
+                    <p>histories: { JSON.stringify(history.histories.map(route => route.url)) }</p>
+                    <p>push: <input type="text" value={ this.state.route.url } onChange={ this.handleChange } /></p>
+                    <p>replace: <input className="replace" type="text" value={ this.state.route.url } onChange={ this.handleChange } /></p>
+                </div>
+            );
+        }
+
+        // 渲染空
+        return null;
+    }
+
+    /* 监听更新 */
+    handleChange(event) {
+        let el = event.target,
+            replace = el.className === 'replace';
+
+        // 更新路径
+        replace ? history.replace(el.value) : history.push(el.value);
     }
 }
 
